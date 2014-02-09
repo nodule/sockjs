b3d46f6261bcc4706f6eadeb40ec2bc3e4602a75
Rob Halff <rob.halff@gmail.com>
2014-02-09 15:58:57 +0100

    update

--- nodes/createServer/node.js
+++ nodes/createServer/node.js
@@ -1,7 +1,11 @@
-var options = input;
-options.sockjsurl = input.url;
-
-var server = sockjs.createServer(options);
+var server = sockjs.createServer({
+  sockjs_url: input.url,
+  prefix: input.prefix,
+  response_limit: input.response_limit,
+  websocket: input.websocket,
+  heartbeat_delay: input.heartbeat_delay,
+  disconnect_delay: input.disconnect_delay
+});
 output = {
   server: server
 };
